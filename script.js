401911db4065f1cefa0c84bcda1f4cece55a7190
Kzuu1023 <delafiora552@gmail.com>
2024-12-30 03:46:33 -0800

    refactor: formatted number

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const mortgageAmount = document.getElementById("amount");
 const termYears = document.getElementById("term");
 const interestRate = document.getElementById("rate");
 const mortgageType = document.querySelectorAll("input[name='mortgage__type']");
+
 const amountError = document.querySelector(".currency__symbol");
 const yearsError = document.querySelector(".years-container");
 const rateError = document.querySelector(".rate__symbol");
@@ -24,15 +25,30 @@ const setSuccess = (input) => {
 
     if (input.value) {
         errorMsg.textContent = "";
-        errorMsg.style.color = "";
         input.parentElement.classList.add("valid");
         input.parentElement.classList.remove("invalid");
         input.parentElement.style.borderColor = "";
     }
 };
 
+// const isValidMortgageType = () => {
+//     let isChecked = false;
+//     for (const types of mortgageType) {
+//         if (types.checked) {
+//             setSuccess(types.parentElement);
+//             types.parentElement.style.borderColor = "var(--lime)";
+//             types.parentElement.style.backgroundColor = "var(--lime-100)";
+//             isChecked = true;
+//         } else {
+//             setError(types.parentElement, "This field is required");
+//         }
+//     }
+//     return isChecked;
+// };
+
 const isValidMortgageType = () => {
     let isChecked = false;
+
     for (const types of mortgageType) {
         if (types.checked) {
             setSuccess(types.parentElement);
@@ -42,22 +58,68 @@ const isValidMortgageType = () => {
         }
     }
 
-    if (!isChecked) {
+    if (!mortgageType.checked) {
         setError(mortgageType[0].parentElement, "This field is required");
     }
-
-    return false;
+    return isChecked;
 };
 
-function calculate() {}
+const validateInputs = (input) => {
+    if (isNaN(input.value)) {
+        console.log("Please enter a valid number");
+    }
+};
+
+function formattedNumber() {
+    // input = input.replace(/[^\d.]/g, "");
+    // if (input.includes(".")) {
+    //     const [integerPart, decimalPart] = input.split(".");
+    //     input =
+    //         parseFloat(integerPart).toLocaleString("en") + "." + decimalPart;
+    // } else {
+    //     input = parseFloat(input).toLocaleString("en");
+    // }
+    // return input;
+
+    const inputText = document.querySelectorAll("input[type='text']");
+
+    inputText.forEach((input) => {
+        input.onkeyup = function () {
+            if (this.value.includes(".")) {
+                const [integerPart, decimalPart] = input.split(".");
+                const removeChar = this.value.replace(/[^\d.]/g, "");
+                this.value = removeChar;
+
+                let formattedNum = this.value.replace(
+                    /\B(?=(\d{3})+(?!\d))/g,
+                    ","
+                );
+                this.value = formattedNum;
+            } else {
+                const removeChar = this.value.replace(/[^\d.]/g, "");
+                this.value = removeChar;
+
+                let formattedNum = this.value.replace(
+                    /\B(?=(\d{3})+(?!\d))/g,
+                    ","
+                );
+                this.value = formattedNum;
+            }
+        };
+    });
+}
+
+formattedNumber();
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
+
     let isValid = true;
 
     if (mortgageAmount.value) {
         setSuccess(mortgageAmount);
         amountError.style.backgroundColor = "";
+        amountError.firstElementChild.style.color = "";
         isValid = true;
     } else {
         setError(mortgageAmount, "This field is required");
@@ -67,8 +129,10 @@ form.addEventListener("submit", function (e) {
     }
 
     if (termYears.value) {
+        validateInputs(termYears);
         setSuccess(termYears);
         yearsError.style.backgroundColor = "";
+        yearsError.firstElementChild.style.color = "";
         isValid = true;
     } else {
         setError(termYears, "This field is required");
@@ -80,6 +144,7 @@ form.addEventListener("submit", function (e) {
     if (interestRate.value) {
         setSuccess(interestRate);
         rateError.style.backgroundColor = "";
+        rateError.firstElementChild.style.color = "";
         isValid = true;
     } else {
         setError(interestRate, "This field is required");
